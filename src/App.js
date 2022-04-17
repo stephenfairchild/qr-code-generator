5bd984f19b14d6aaa88a9b9ff69e23a2558181b7
Stephen Fairchild <stephen.fairchild@researchsquare.com>
2022-04-17 09:37:49 -0400

    prototype qr code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+
+import QRCode from "react-qr-code";
 
 function App() {
+  const [ email, setEmail ] = useState("");
+  const [ secondaryEmail, setSecondaryEmail ] = useState("");
+  const [ showCode, setShowCode ] = useState(false);
+  const [ mail, setMail ] = useState(false);
+  const [ subject, setSubject ] = useState("");
+
+
+    function generateCode() {
+        let tmpMail = "";
+
+        if (!email) {
+            alert("A primary email must be set");
+        }
+
+        if (!subject) {
+            alert("A subject line is required");
+        }
+
+        tmpMail = email;
+
+        if (secondaryEmail) {
+            tmpMail = `${tmpMail},${secondaryEmail}`;
+        }
+
+        setMail(tmpMail);
+
+        if (showCode) {
+            setShowCode(false);
+        }
+        
+        setTimeout(() => {
+            setShowCode(true);
+        }, 2000)
+    }
+
+    function download() {
+        const svg = document.getElementById("QRCode");
+        const svgData = new XMLSerializer().serializeToString(svg);
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        const img = new Image();
+        img.onload = () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0);
+          const pngFile = canvas.toDataURL("image/png");
+          const downloadLink = document.createElement("a");
+          downloadLink.download = "QRCode";
+          downloadLink.href = `${pngFile}`;
+          downloadLink.click();
+        };
+        img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <label>
+                Email: 
+                <input value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+        </div>
+        <div>
+            <label>
+                Secondary Email: 
+                <input 
+                    value={secondaryEmail} 
+                    onChange={(e) => setSecondaryEmail(e.target.value)} />
+            </label>
+        </div>
+        <div>
+            <label>
+                Subject Line:
+                <input 
+                    value={subject} 
+                    onChange={(e) => setSubject(e.target.value)} />
+            </label>
+        </div>
+        <button onClick={() => generateCode()}>Generate code</button>
+
+        { showCode && 
+        <div>
+            <div style={{ background: 'white', padding: '16px' }}>
+                <QRCode id="QRCode" value={`mailto:${mail}?subject=${subject}`} />
+            </div> 
+            <button onClick={() => download()}>Download</button>
+        </div>
+        }
     </div>
   );
 }
